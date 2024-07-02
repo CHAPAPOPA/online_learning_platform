51aa276937fc08f96109e59256bc17fdc08e65a5
CHAPAPOPA <yackow.muliawin2015@yandex.ru>
2024-07-02 14:29:57 +0300

    Task 24.1 completed

---
+++ users/models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Введите почту"
+    )
+    phone = models.CharField(
+        max_length=11,
+        verbose_name="Телефон",
+        help_text="Введите номер телефона",
+        **NULLABLE
+    )
+    city = models.CharField(
+        max_length=50, verbose_name="Город", help_text="Укажите город", **NULLABLE
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatar/",
+        verbose_name="Аватар",
+        help_text="Загрузите аватар",
+        **NULLABLE
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        ordering = ("id",)
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
